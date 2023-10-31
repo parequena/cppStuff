7c75b9b1a3240d6446e5b7896d5fcc6c07a30699
Danae Requena <danaerequena@MacBook-Pro-de-Danae.local>
2023-10-31 16:15:48 +0100

    Support of multiple execs rust

--- fork/rustChild.rs
+++ fork/rustChild.rs
@@ -1,4 +1,19 @@
+use std::env;
+
 fn main()
 {
-   println!("Hello from rust!");
+   let args: Vec<String> = env::args().collect();
+   let args_size = args.len();
+
+   let mut n_times = 1;
+   if args_size > 1
+   {
+      n_times = args[1].parse().unwrap();
+   }
+
+   let indexes = 0..(n_times);
+   for i in indexes
+   {
+      println!("{} | Hello from rust", i)
+   }
 }
